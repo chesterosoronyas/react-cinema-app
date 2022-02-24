a729d53d01ea6a0deea60a0cba19cb75dca03345
chesterosoronyas <31725146+chesterosoronyas@users.noreply.github.com>
2022-02-24 12:35:42 +0300

    changed app component function to arrow function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +19,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
